9eb90671c60a887203678e4f90d60cabbdd88a69
Ayaka Matsuoka <a-matsuoka@nri.co.jp>
2021-11-12 02:03:05 +0900

    [add]フロント

--- public/js/controllers/CountryController.js
+++ public/js/controllers/CountryController.js
@@ -3,11 +3,30 @@ app.controller("CountryController", [
   "$scope",
   "$http",
   function($scope, $http) {
-    $scope.user = {};
+    $scope.getUser = function(userIndex) {
+      $scope.messageTarget = "user";
+      $scope.currentUser = $scope.users[userIndex];
+      $http
+        .get(
+          `http://localhost:3000/api/users/${$scope.currentUser.id}/messages?fromId=${$scope.user.id}`
+        )
+        .then((response) => {
+          $scope.currentMessages = response.data;
+        });
+    };
 
-    const getLocate = function() {
-      $http.get("http://localhost:3000/api/location").then((response) => {
-        $scope.users = response.data;
+    $scope.signIn = function() {
+      const postData = { username: $("#username-input")[0].value };
+      $http({
+        method: "POST",
+        url: "http://localhost:3000/api/users",
+        data: postData,
+        headers: { "Content-Type": "application/json" },
+      }).then((response) => {
+        $scope.usernameRecieved = true;
+        $scope.user = response.data;
+        getUsers();
+        getChannels();
       });
     };
   },
